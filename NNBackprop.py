942ab3adf41b8aca8316318c8cd921e43d29a45b
Michael James Hon <mhon2014@my.fit.edu>
2019-11-17 13:52:45 -0500

    added python file

---
+++ NNBackprop.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+class NeuralNetwork:
+    def __init__(self):
+        # np.random.seed(1) ????
+        self.weights = 0 #init weights
+        self.bias = 0 #init biases
+
+    def activation(self):
+        pass
+    def train(self):
+        pass
+    def predict(self):
+        pass
+
+
+
+if __name__ == "__main__":
+    print("LESGOOOOOO")
+    pass
