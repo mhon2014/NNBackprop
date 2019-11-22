c66e9e3e8391443aeddb8dc6c1501098d53aa038
Michael James Hon <mhon2014@my.fit.edu>
2019-11-22 14:14:39 -0500

    removed test file and updated main

--- jupytermain.py
+++ jupytermain.py
@@ -3,61 +3,23 @@
 # %% Prep
 import numpy as np
 import matplotlib.pyplot as plt
-import math
+from math import sin, pi
 import random
-import csv
-import numpy
-import pathlib
+import pandas as pd
+from NNBackprop import NeuralNetwork
 
 def function(x1, x2):
-    '''
-    Input: 2 decimal numbers from 0 to 1 inclusive
-    Ouput: 1 decimal number
-    time: O(1), space: O(1)
-    '''
-    return (math.sin(2*math.pi*x1) * math.sin(2*math.pi*x2))
+    '''Function that the neural net is trying to learn'''
+    return (sin(2*pi*x1) * sin(2*pi*x2))
 
-
-def genData(filename, n=5):
-    '''
-    Input: filename to store data in, function to generate the data,
-            n for generating n + 1 points with
-    Output: None
-    time: O((n + 1)^2), space: O((n + 1)^2)
-    '''
-
-    with open(filename, 'w+') as F:
-        for i in range(n + 1):
-            for j in range(n + 1):
-                x1 = i/n
-                x2 = j/n
-                Y = function(x1, x2)
-                F.write(f'{x1:.2f},{x2:.2f},{Y:.2f}\n')
-
-
-def genRandData(filename, n=50):
-    '''
-    Input: filename to store data in, function to generate the data,
-            n for generating n + 1 points with
-    Output: None
-    time: O((n + 1)^2), space: O((n + 1)^2)
-    '''
-
-    with open(filename, 'w+') as F:
-        for _ in range(n):
-            x1 = random.random()
-            x2 = random.random()
+def genData(dataset, n=5):
+    '''Function that generates a dataset  based on the n and store it in the list that is passed'''
+    for i in range(n + 1):
+        for j in range(n + 1):
+            x1 = i/n
+            x2 = j/n
             Y = function(x1, x2)
-            F.write(f'{x1:.2f},{x2:.2f},{Y:.2f}\n')
-
-
-def parseData(filename, dataset):
-    '''read data from file and insert it into a cached dataset'''
-    with open(filename, 'r') as F:
-        for line in F:
-            x1, x2, Y = line.split(',')
             dataset.append((float(x1), float(x2), float(Y)))
-
 
 def scramble(old):
     '''return a scrambled list based on old list'''
@@ -66,246 +28,119 @@ def scramble(old):
     return new
 
 
-def dispLoss(errorList, epochList):
-    plt.plot(epochList, errorList)
-    plt.xlabel('Number of Epoch')
-    plt.ylabel('Mean Square Error')
-    plt.show()
+def genRandData(dataset, n=10):
+    '''Generate random data and store it'''
 
+    for _ in range(n):
+        x1 = random.random()
+        x2 = random.random()
+        Y = function(x1, x2)
+        dataset.append((float(x1), float(x2), float(Y)))
 
-def avg(pylist):
-    '''return the average of list'''
-    return sum(pylist) / len(pylist)
+    dataset = np.round(dataset)
 
-
-def tabulate(csvfile, x1, x2, Y, t):
-    with open(csvfile, mode='a+') as F:
-        F_writer = csv.writer(
-            F, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        F_writer.writerow([x1, x2, t, Y])
-
-
-class NeuralNetwork:
-    def __init__(self, HL = 2):
-        self.W1 = np.zeros((HL,2)) #initialize weights to first layer
-        self.bLayer = np.zeros((HL,1)) #initialize hidden layer bias
-        self.W2 =  np.zeros((1,HL)) #initialize weights to output
-        self.bOutput = np.zeros((1,1)) #initialize output bias
-
-        self.P = None #input
-        self.outputlayer = None #output of hidden layer
-        self.A = None # final output
-        self.n1 = None #net output N = WP n1 = w11p1 + w21p2
-        self.n2 = None #net output of last layer
-
-        print('Initial Weights and Biases')
-        print(f'w1 = {self.W1}')
-        print(f'b1 = {self.bLayer}')
-        print(f'w2 = {self.W2}')
-        print(f'b2 = {self.bOutput}')
-
-    def s1(self, t, s2):
-        ''' sensitivity of first layer '''
-        return self.dsig(self.n1) @ np.transpose(self.W2) @ s2
-
-    def s2(self, t):
-        ''' sensitivity of last layer -2*F(n)*E '''
-        return -2 * (self.dlin(self.n2) @ self.error(t, self.A))
-
-    def error (self, t, A):
-        ''' error function '''
-        return (t - A)
-
-    def sig(self, n):
-        '''Sigmoid Activation Function'''
-        return 1 / (1 + np.exp(-n))
-
-    def dsig(self, n):
-        '''Derivative of last layer which is the diagonals only'''
-        res = np.zeros((n.size, n.size))
-
-        for i in range (n.size):
-            res[i][i] = self.sig(n[i][0]) * (1 - self.sig(n[i][0]))
-
-        return res
-
-    def lin(self, n):
-        '''pure line function'''
-        return n
-
-    def dlin(self, n):
-        '''derivative of linear function which is just the identity matrix'''
-        return np.identity(n.size)
-
-
-    def feedfoward(self, P):
-        ''' feed foward function to compute layers and return the output '''
-        self.P = P
-        self.n1 = (self.W1 @ self.P) + self.bLayer
-        self.outputlayer = self.sig(self.n1)
-        self.n2 = (self.W2 @ self.outputlayer) + self.bOutput
-        self.A = self.lin(self.n2)
-
-        return self.A
-
-    def backpropagrate(self, t, alpha=0.5):
-        '''weights update'''
-        s2 = self.s2(t)
-        s1 = self.s1(t, s2)
-
-        '''Wnew = Wold + (-alpha)*S(A_transpose)  '''
-        self.W2 = self.W2 + -alpha * (s2 @ np.transpose(self.outputlayer))
-        self.bOutput = self.bOutput + -alpha * s2
-        self.W1 = self.W1 + -alpha * (s1 @ np.transpose(self.P))
-        self.bLayer = self.bLayer + -alpha * s1
-
-
-    def train(self, dataset, epoch = 1000, printFlag = False):
-        ''' train network '''
-        errorAvgMin = 1
-        epochList = []
-        errorList = []
-        errorSum = 0
-        count = 0
-
-        for i in range (epoch):
-            for x1, x2, t in dataset:
-                p = np.array([[x1], [x2]])
-                self.feedfoward(p)
-                ''' loss function returns 1x1 vector so used [0][0] '''
-                errorSum += self.Loss(t, self.A)[0][0]
-
-                self.feedfoward(p)
-
-                if (printFlag):
-                    tabulate('training.csv', x1, x2, (self.A), t)
-
-                count += 1
-                self.backpropagrate(t)
-
-            epochList.append(i+1)
-            errorAvg = errorSum / count
-            errorList.append(errorAvg)
-            
-            if errorAvg < errorAvgMin:
-                errorAvgMin = errorAvg
-
-            errorSum = 0 #reset sum
-            count = 0 #reset count
-
-        print('Final Weights and Biases')
-        print(f'W1 = {self.W1}')
-        print(f'bLayer = {self.bLayer}')
-        print(f'W2 = {self.W2}')
-        print(f'bOutput = {self.bOutput}')
-
-        # displaying mean square error vs the epoch number
-        print(f'Min MSE: {errorAvgMin}, Avg MSE: {avg(errorList)}')
-        dispLoss(errorList, epochList)
-    
-    def predict(self, x1, x2):
-        ''' predict the output based on input '''
-        p = np.array([[x1], [x2]])
-        A = self.feedfoward(p)
-
-        return A   
-
-    def Loss(self, t, A):
-        ''' return the error squared  '''
-        MSE = (t - A)**2
-        
-        return MSE
-
-    def test(self, testdata, testfile = 'data/test', printFlag = False):
-        ''' test the performance'''
-        count = 0
-        if(print):
-            for x1, x2, t in testdata:
-                A = self.predict(x1,x2)
-                error = self.Loss(t, A)
-                tabulate('data/test.csv', x1, x2, A, error)
-        else :
-            for x1, x2, t in testdata:
-                A = self.predict(x1,x2)
-                error = self.Loss(t, A)
-                if count < 3:
-                    print(f'In: ({x1},{x2}) Out: {A:.2f}, Target: {t}, Err: {error:.2f}')
-                count += 1
-    
-# %% Case 1 : i/5 j/5 Training
-'''Case 1'''
-
-datafile = 'data/data5/trainingdata.txt'
-testfile = 'data/data5/test.txt'
-
-genData(datafile, 5)
-
-genRandData(testfile, 50)
+# %% Part A : i/5 j/5 generate data
+''' Part A'''
 
 dataset = []
-parseData(datafile, dataset)
+genData(dataset)
+
 dataset = scramble(dataset)
 
+# %% Train for 1000 epoch
+lastIteration = []    #last iteration of training input and output
+
+NN = NeuralNetwork(3)  # create NN with 3 hidden units
+
+NN.train(dataset, lastIteration, 1000)  # train the neural network
+
+li = pd.DataFrame(lastIteration, columns = ['x1', 'x2', 'A', 't', 'error'])
+
+print(li)
+
+li.to_excel("/data/1000epoch/train.xlsx")
 # print(dataset)
 
-# %% Testing 1
+# %% Train for 10000 epoch
+lastIteration = []    #last iteration of training input and output
 
-NN = NeuralNetwork()  # create NN with 3 hidden neurons
+NN = NeuralNetwork(3)  # create NN with 3 hidden units
 
-NN.train(dataset)  # train the neural network
+NN.train(dataset, lastIteration, 10000)  # train the neural network
+
+li = pd.DataFrame(lastIteration, columns = ['x1', 'x2', 'A', 't', 'error'])
+
+print(li)
+
+li.to_excel("/data/10000epoch/train.xlsx")
+
+# %% test on i/10 j/10 generate data
 
 testset = []
-parseData(testfile, testset)
+genData(testset, 10)
 
-NN.test(testset,'data/data5/test.csv', True)
+# %% test for 1000 epoch
+testlist = []
 
-# %% Case 2 i/10 j/10 Training
-'''Case 2 '''
+NN.test(testset,testlist)  # test the neural network
 
-datafile = './data/trainingdata10.txt'
-testfile = './data/test.txt'
+tl = pd.DataFrame(testlist, columns = ['x1', 'x2', 'A', 't', 'error'])
 
-genData(datafile, 10)
-genRandData(testfile, 50)
+# %% test for 10000 epoch
+
+testlist = []
+
+NN.test(testset,testlist)  # test the neural network
+
+tl = pd.DataFrame(testlist, columns = ['x1', 'x2', 'A', 't', 'error'])
+
+# %% Part C random x1, and x2
+'''Part C'''
 
 dataset = []
-parseData(datafile, dataset)
-dataset = scramble(dataset)
+genRandData(dataset, 5)
 
-# print(dataset)
-# %% Testing 2
-NN = NeuralNetwork()  # create NN with 3 hidden neurons
+# %% Train for 1000 epoch
+lastIteration = []    #last iteration of training input and output
 
-NN.train(dataset, 10000)  # train the neural network
+NN = NeuralNetwork(3)  # create NN with 3 hidden units
 
-testset = []
-parseData(testfile, testset)
+NN.train(dataset, lastIteration, 1000)  # train the neural network
 
-NN.test(testset)
+li = pd.DataFrame(lastIteration, columns = ['x1', 'x2', 'A', 't', 'error'])
 
-
-# %% Case 3 random x1, and x2 Training
-'''Case 3'''
+print(li)
 
 
-datafile = './data/dataR.txt'
-testfile = './data/testR.txt'
+# %% Train for 10000 epoch
+lastIteration = []    #last iteration of training input and output
 
-genRandData(datafile, 100)
-genRandData(testfile, 50)
+NN = NeuralNetwork(3)  # create NN with 3 hidden units
 
-dataset = []
-parseData(datafile, dataset)
-dataset = scramble(dataset)
+NN.train(dataset, lastIteration, 10000)  # train the neural network
 
-# print(dataset)
-# %% Testing 3
+li = pd.DataFrame(lastIteration, columns = ['x1', 'x2', 'A', 't', 'error'])
 
-NN = NeuralNetwork()  # create NN with 2 hidden neurons
+print(li)
 
-NN.train(dataset, 10000)  # train the neural network
+# %% test on i/10 j/10 
 
 testset = []
-parseData(testfile, testset)
+genData(testset, 10)
 
-NN.test(testset)
+# %% test for 1000 epoch
+
+testlist = []
+
+NN.test(testset,testlist)  # test the neural network
+
+tl = pd.DataFrame(testlist, columns = ['x1', 'x2', 'A', 't', 'error'])
+
+# %% test for 10000 epoch
+
+testlist = []
+
+NN.test(testset,testlist)  # test the neural network
+
+tl = pd.DataFrame(testlist, columns = ['x1', 'x2', 'A', 't', 'error'])
+
